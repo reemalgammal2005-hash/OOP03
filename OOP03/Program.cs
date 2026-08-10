622c4d9ea8dba44851b8b38f2caf7f7df14746a1
Reemalgammal2005 <reemalgammal2005@gmail.com>
2026-08-11 00:38:40 +0300

    Part 1 => Q1 & Q2

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 — Theoretical Questions
+            #region Q 1
+            /* A- method override (use key word (override - virtual) , loak at object)
+             * method overloading ( use key word new , loak at refrance)
+             * 
+             * B- static ( early , overloading , comile time ,refrance type , fast)
+             * dynamic ( late , override , run time , object , slower )
+             */
+            #endregion
+
+            #region Q 2
+            /* A- sealed class cannot be inherited from
+             * B-sealed class prevents inheritance, while a sealed method prevents further overriding
+             * C-No, because a sealed method cannot be overridden again
+             */
+            #endregion
+            #endregion
         }
     }
 }
